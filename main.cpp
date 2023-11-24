84ec8adad959c1ea4089a0a381307df392682e7b
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-11-24 13:37:57 -0700

    added testcase

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 
 int main() {
     try {
-        int a[] = {50, 700, 76, 85, 73, 12};
-        int n = sizeof(a)/sizeof(a[0]);
+
+        // TestCase - 1
+        int a[] = {50, 700, 76, 85, 73, 101, 92, 1};
         Hash h1(7);
         Hash h2(7);
 
+        /* TestCase - 2
+        int a[] = {1, 11, 21, 31, 41, 51, 61, 71, 81, 91};
+        Hash h1(10);
+        Hash h2(10);
+
+        // TestCase - 3
+        int a[] = {50, 700, 76, 85, 73, 101, 92, 1};
+        Hash h1(7);
+        Hash h2(7);
+
+        // TestCase - 4
+        int a[] = {1, 2, 3, 4, 5};
+        Hash h1(5);
+        Hash h2(5);
+        */
+
+        int n = sizeof(a)/sizeof(a[0]);
         for (int i=0; i < n; ++i) {
             quadratic(h1, a[i]);
             chain(h2, a[i]);
@@ -15,9 +33,6 @@ int main() {
 
         h1.displayHash();
         h2.displayHash();
-
-        quadratic(h1, 92);
-        h1.displayHash();
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
     }
